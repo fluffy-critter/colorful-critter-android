4c43d8c048465de930eee85beff5c2d5487eafc7
fluffy <fluffy@beesbuzz.biz>
2017-09-20 10:57:22 -0700

    Update to NDK r15

--- love/src/jni/Application.mk
+++ love/src/jni/Application.mk
@@ -1,8 +1,8 @@
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static 
+# APP_STL := stlport_static
 APP_STL := gnustl_shared
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi-v7a
 APP_CPPFLAGS := -frtti
 APP_LDFLAGS := -llog -landroid -lz
 APP_PLATFORM := 10
--- love/src/jni/SDL2-2.0.5/android-project/jni/Application.mk
+++ love/src/jni/SDL2-2.0.5/android-project/jni/Application.mk
@@ -1,10 +1,10 @@
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static 
+# APP_STL := stlport_static
 
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi-v7a
 
 # Min SDK level
-APP_PLATFORM=android-10
+APP_PLATFORM=android-14
 
